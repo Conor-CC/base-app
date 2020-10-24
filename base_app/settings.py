d382d376574004da8b76520d64fcc0244e249c6b
Conor-CC <cleryc@tcd.ie>
2020-10-24 19:46:35 +0100

    Basic password reset

--- base_app/settings.py
+++ base_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'django_rest_passwordreset',
 
     'user',
     'authentication',
@@ -122,6 +123,10 @@ AUTHENTICATION_BACKENDS = {
 
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'authentication/templates'),
+)
+
 
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'user.serializers.UserSerializer_Create'
