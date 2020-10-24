7ed8af85b2b780c4d810a09c382ac4a3dc48ac92
Conor-CC <cleryc@tcd.ie>
2020-10-24 17:56:08 +0100

    Refactored base_app urls

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,25 +1,14 @@
-"""user_models URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from dj_rest_auth.registration.views import VerifyEmailView
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from user import urls as user_urls
+from authentication import urls as auth_urls
+
+app_urls = [
+    path('user/', include(user_urls)),
+    path('auth/', include(auth_urls)),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include(user_urls)),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/v1/', include(app_urls)),
 ]
