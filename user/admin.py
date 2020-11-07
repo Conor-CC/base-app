f67b60d16c0bd1fd8d42d0b7cdaf227c057e5305
Conor-CC <cleryc@tcd.ie>
2020-11-07 12:59:29 +0000

    Registered User models for Admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from user.models import (UserModel_Base, UserModel_ProfileTypeOne,
+                         UserModel_ProfileTypeTwo, UserModel_ProfileTypeThree)
 
-# Register your models here.
+
+@admin.register(UserModel_Base)
+class UserModel_BaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_on', 'updated_on', 'uuid', 'username',
+                    'email', 'first_name', 'last_name', 'organization',
+                    'user_type')
+
+
+@admin.register(UserModel_ProfileTypeOne)
+class UserModel_ProfileTypeOneAdmin(admin.ModelAdmin):
+    list_display = ('user', 'info_user_type_one')
+
+
+@admin.register(UserModel_ProfileTypeTwo)
+class UserModel_ProfileTypeTwoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'info_user_type_two')
+
+
+@admin.register(UserModel_ProfileTypeThree)
+class UserModel_ProfileTypeThreeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'info_user_type_three')
